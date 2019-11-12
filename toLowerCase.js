c45c9e22e5b683d65abb62d4fbf47af2c973aa78
Ryo Togashi <35892492+ryotogashi@users.noreply.github.com>
2019-11-11 20:28:21 -0800

    add some programming questions.

---
+++ toLowerCase.js
@@ -0,0 +1,11 @@
+// 半角アルファベットの小文字で構成された長さ n の文字列 s が与えられます。
+// 文字列 s を大文字に変換して出力して下さい。
+
+// 入力例1
+// paiza
+// 出力例1
+// PAIZA
+// 入力例2
+// abcdefghijklmnopqrstuvwxyz
+// 出力例2
+// ABCDEFGHIJKLMNOPQRSTUVWXYZ
