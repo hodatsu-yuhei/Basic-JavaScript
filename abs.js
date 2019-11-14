b320633fe97458554fcbc87c94423bd839797cc3
hodatsu-yuhei <y.houdatsu@gmail.com>
2019-11-14 09:03:09 +0900

    Basic-JavaScript

--- abs.js
+++ abs.js
@@ -5,3 +5,19 @@
 // と入力された場合
 // 10
 // と出力して下さい。
+
+<script>
+'use strict'
+
+var.result = getRandom(-100,100);
+console.log(result);
+
+function.getRandom(min,max){
+    var random = Math.floor(Math.Random() * (max + 1 - min)) + min;
+
+    return random;
+}
+
+
+
+</script>
